431d0cba64016d5f5676c0d7462efe7cd670396a
Sergej Bobusky <bobuskysergej@gmail.com>
2025-11-29 19:25:25 +0100

    init commit

--- src/app/(main)/prompts/template/template-filters.test.tsx
+++ src/app/(main)/prompts/template/template-filters.test.tsx
@@ -1,6 +1,7 @@
 import { screen, waitFor } from "@testing-library/dom";
 import { render } from "@testing-library/react";
-import { assertInDocument } from "@tests";
+import userEvent from "@testing-library/user-event";
+import { assertInDocument, getElementById } from "@tests";
 
 import { TemplateFilters } from "./template-filters";
 
@@ -35,5 +36,43 @@ describe("TemplateFilters rendering tests", () => {
       });
 
       expect(container).toMatchSnapshot();
+   });
+});
+
+describe("TemplateFilters functionality tests", () => {
+   it("TemplateFilters - search template - test", async () => {
+      const loadedCategories = ["category 1", "category 2", "category 3"];
+      const categories: string[] = [];
+      const search = "";
+      const setSearchFn = jest.fn();
+
+      render(
+         <TemplateFilters
+            loadedCategories={loadedCategories}
+            search={search}
+            categories={categories}
+            setSearch={setSearchFn}
+            setCategories={jest.fn()}
+         />
+      );
+
+      await waitFor(() => {
+         assertRendered();
+         expect(setSearchFn).not.toHaveBeenCalled();
+      });
+
+      const serachValue = "test 1";
+      const input = getElementById("search-templates");
+      userEvent.type(input, serachValue);
+
+      await waitFor(() => {
+         expect(setSearchFn).toHaveBeenCalledTimes(6);
+         expect(setSearchFn).toHaveBeenNthCalledWith(1, "t");
+         expect(setSearchFn).toHaveBeenNthCalledWith(2, "e");
+         expect(setSearchFn).toHaveBeenNthCalledWith(3, "s");
+         expect(setSearchFn).toHaveBeenNthCalledWith(4, "t");
+         expect(setSearchFn).toHaveBeenNthCalledWith(5, " ");
+         expect(setSearchFn).toHaveBeenNthCalledWith(6, "1");
+      });
    });
 });
