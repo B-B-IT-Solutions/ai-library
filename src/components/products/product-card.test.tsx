b7685aac7e5fbd1ae4292360bde228a9881e290c
Sergej Bobusky <bobuskysergej@gmail.com>
2025-12-17 15:12:55 +0100

    init commit

---
+++ src/components/products/product-card.test.tsx
@@ -0,0 +1,48 @@
+import { screen, waitFor } from "@testing-library/dom";
+import { render } from "@testing-library/react";
+import { assertInDocument, dtestData } from "@tests";
+
+import { ProductCard } from "./product-card";
+
+const assertRendered = () => {
+   const card = screen.getByTestId("product-card");
+   const addToCartBtn = screen.getByTestId("add-to-cart-btn");
+   const viewDetailsBtn = screen.getByTestId("view-details-btn");
+
+   assertInDocument(card);
+   assertInDocument(addToCartBtn);
+   assertInDocument(viewDetailsBtn);
+};
+
+describe("ProductCard rendering tests", () => {
+   it("ProductCard - product BUNDLE - test", async () => {
+      const product = dtestData.dProduct();
+      product.type = "BUNDLE";
+      product.template = undefined;
+
+      const { container } = render(
+         <ProductCard product={product} isInCart={false} />
+      );
+
+      await waitFor(() => {
+         assertRendered();
+      });
+
+      expect(container).toMatchSnapshot();
+   });
+
+   it("ProductCard- product TEMPLATE - test", async () => {
+      const product = dtestData.dProduct();
+      product.type = "TEMPLATE";
+
+      const { container } = render(
+         <ProductCard product={product} isInCart={false} />
+      );
+
+      await waitFor(() => {
+         assertRendered();
+      });
+
+      expect(container).toMatchSnapshot();
+   });
+});
