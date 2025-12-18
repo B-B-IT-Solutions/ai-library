84f603d2e1b8247d670b3e25ae89b65bc6721f1c
Sergej Bobusky <bobuskysergej@gmail.com>
2025-12-18 02:05:34 +0100

    init commit

--- src/components/products/product-details-dialog.tsx
+++ src/components/products/product-details-dialog.tsx
@@ -137,7 +137,6 @@ export const ProductDetailsDialog: FC<ProductDetailsDialogProps> = ({
 
    return (
       <Dialog
-         key={`product-details-${open}`}
          open={open}
          onOpenChange={onClose}
          data-testid="product-detials-dialog"
