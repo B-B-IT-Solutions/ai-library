2702d2a6cca45437e8bbb7b1efad63173025f049
Sergej Bobusky <bobuskysergej@gmail.com>
2025-12-24 19:59:13 +0100

    init commit

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,8 +1,7 @@
 export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || "KI Bibliothek";
 export const APP_DESCRIPTION =
    process.env.NEXT_PUBLIC_APP_DESCRIPTION || "Modernene KI-Bibliothek";
-export const APP_URL =
-   process.env.NEXT_PUBLIC_APP_URL || "http://localhost:3000";
+export const APP_URL = process.env.NEXT_PUBLIC_APP_URL;
 
 export const STRIPE_SECRET_KEY = process.env.STRIPE_SECRET_KEY;
 export const STRIPE_WEBHOOK_SECRET = process.env.STRIPE_WEBHOOK_SECRET;
