5148a44b65d98afee07b1c9cf91e5ce01ebac782
Sergej Bobusky <bobuskysergej@gmail.com>
2026-01-01 17:01:35 +0100

    init commit

--- src/data/services/order/order.service.ts
+++ src/data/services/order/order.service.ts
@@ -1,6 +1,6 @@
 import { validate as isValidUuid } from "uuid";
 
-import { OrderRepository } from "@/data/db/queries/order";
+import { OrderRepository, OrderUpdate } from "@/data/db/queries/order";
 import { CartService } from "@/data/services/cart";
 import { DOrder } from "@/data/types/domain/order";
 import { requireUser } from "../../actions/auth-utils";
@@ -61,21 +61,23 @@ export class OrderService {
       }
 
       if (order.status !== "COMPLETED") {
-         await this.orderRepository.pUpdateOrder(orderId, {
+         const payload: OrderUpdate = {
             status: "COMPLETED",
             stripePaymentIntentId: paymentIntentId,
             stripePaymentStatus: paymentStatus,
             paymentMethod: "STRIPE",
-         });
+         };
+         await this.orderRepository.pUpdateOrder(orderId, payload);
          await this.libraryService.createLibraryEntries(order);
          await this.cartService.clearCart(order.userId);
       }
    }
 
    async handleStripeCheckoutExpired(orderId: string) {
-      await this.orderRepository.pUpdateOrder(orderId, {
+      const payload: OrderUpdate = {
          status: "FAILED",
-      });
+      };
+      await this.orderRepository.pUpdateOrder(orderId, payload);
    }
 
    async handleStripePaymentFailed(paymentIntentId: string) {
@@ -89,9 +91,10 @@ export class OrderService {
          );
       }
 
-      await this.orderRepository.pUpdateOrder(order.id, {
+      const payload: OrderUpdate = {
          status: "FAILED",
          stripePaymentStatus: "failed",
-      });
+      };
+      await this.orderRepository.pUpdateOrder(order.id, payload);
    }
 }
