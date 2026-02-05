2e02b4aa336eebfc9c888a3b194f1c7c9fb6c11d
Sergej Bobusky <bobuskysergej@gmail.com>
2026-02-05 20:15:26 +0100

    init commit

--- src/data/actions/auth-utils.test.ts
+++ src/data/actions/auth-utils.test.ts
@@ -3,7 +3,7 @@ import { AuthMockedFunction, ntestData } from "@tests";
 import { auth } from "@/auth";
 import { LoginUser } from "../types/next-auth";
 
-import { requireUser } from "./auth-utils";
+import { isAuthenticated, requireUser } from "./auth-utils";
 
 const authMock = auth as unknown as AuthMockedFunction;
 
@@ -47,5 +47,43 @@ describe("requireUser tests", () => {
          email: session.user.email,
       };
       expect(user).toEqual(expectedUser);
+   });
+});
+
+describe("isAuthenticated tests", () => {
+   beforeEach(() => {
+      jest.resetAllMocks();
+   });
+
+   it("isAuthenticated - session null - test", async () => {
+      authMock.mockResolvedValue(null);
+      const result = await isAuthenticated();
+      expect(result).toBe(false);
+   });
+
+   it("isAuthenticated - session.user undefined - test", async () => {
+      const session = ntestData.session();
+      session.user = undefined;
+      authMock.mockResolvedValue(session);
+
+      const result = await isAuthenticated();
+      expect(result).toBe(false);
+   });
+
+   it("isAuthenticated - session.user.id undefined - test", async () => {
+      const session = ntestData.session();
+      session.user.id = undefined;
+      authMock.mockResolvedValue(session);
+
+      const result = await isAuthenticated();
+      expect(result).toBe(false);
+   });
+
+   it("isAuthenticated - session.user.id defined - test", async () => {
+      const session = ntestData.session();
+      authMock.mockResolvedValue(session);
+
+      const result = await isAuthenticated();
+      expect(result).toBe(true);
    });
 });
