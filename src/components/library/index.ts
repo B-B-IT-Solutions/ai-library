f0e0dba7195c61073a92903cd35af80bfc6cd399
Sergej Bobusky <bobuskysergej@gmail.com>
2026-02-19 15:33:00 +0100

    init commit

--- src/components/library/index.ts
+++ src/components/library/index.ts
@@ -1,4 +1,3 @@
-export { Library } from "./library";
 export { LibraryDashboard } from "./library-dashboard";
 export { LibraryEntryDetails } from "./library-entry/library-entry-details";
 export { NewLibraryEntry } from "./create-template/library-entry-new";
