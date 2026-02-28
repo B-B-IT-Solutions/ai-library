aa04dea79c7210368e669899aa2d429db1825209
Sergej Bobusky <bobuskysergej@gmail.com>
2026-02-28 21:46:34 +0100

    init commit

--- src/data/repositories/prompt-template/prompt.template.mapper.ts
+++ src/data/repositories/prompt-template/prompt.template.mapper.ts
@@ -53,9 +53,7 @@ export const toDPromptTemplate = (
       content: prompt.content,
       detailedDescription: prompt.detailedDescription,
       fields: toDTemplateFields(prompt.fields),
-      globalFieldIds: prompt.globalFields
-         .sort((a, b) => a.order - b.order)
-         .map((gf) => gf.globalFieldId),
+      globalFieldIds: map(prompt.globalFields, (gf) => gf.globalFieldId),
       updatedAt: prompt.updatedAt.toISOString(),
       createdAt: prompt.createdAt.toISOString(),
    };
