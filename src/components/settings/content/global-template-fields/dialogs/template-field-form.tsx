c2102090f44a864d4550b51c07689ffda992b59a
Sergej Bobusky <bobuskysergej@gmail.com>
2026-03-03 14:14:45 +0100

    init commit

--- src/components/settings/content/global-template-fields/dialogs/template-field-form.tsx
+++ src/components/settings/content/global-template-fields/dialogs/template-field-form.tsx
@@ -11,6 +11,8 @@ import {
    TemplateFieldSelectOptions,
    TemplateFieldType,
 } from "@/components/shared/template-fields";
+import { FormSelect } from "@/components/shared/widgets";
+import { DPromptTemplateFieldType } from "@/data/types/domain/prompt.template";
 import { DGlobalTemplateFieldUpdate } from "@/data/types/domain/settings";
 
 type Props = {
@@ -18,10 +20,42 @@ type Props = {
    control: Control<DGlobalTemplateFieldUpdate>;
 };
 
-const OPTION_TYPES = ["SELECT", "RADIO"];
+const OPTION_TYPES: DPromptTemplateFieldType[] = ["SELECT", "RADIO"];
 
 export const GlobalTemplateFieldForm = ({ watch, control }: Props) => {
    const type = watch("type");
+   const options = watch("options") ?? [];
+
+   const renderDefaultValue = () => {
+      if (OPTION_TYPES.includes(type)) {
+         return (
+            <FormSelect<DGlobalTemplateFieldUpdate>
+               name="defaultValue"
+               label="Standardwert"
+               placeholder="Standardwert auswählen"
+               options={options}
+               control={control}
+            />
+         );
+      }
+      return (
+         <TemplateFieldDefaultValue<DGlobalTemplateFieldUpdate>
+            name="defaultValue"
+            control={control}
+         />
+      );
+   };
+
+   const renderSelectedOptions = () => {
+      if (OPTION_TYPES.includes(type)) {
+         return (
+            <TemplateFieldSelectOptions<DGlobalTemplateFieldUpdate>
+               name="options"
+               control={control}
+            />
+         );
+      }
+   };
 
    return (
       <div className="grid grid-cols-2 gap-4" data-testid="template-field-form">
@@ -38,16 +72,8 @@ export const GlobalTemplateFieldForm = ({ watch, control }: Props) => {
             name={"type"}
             control={control}
          />
-         <TemplateFieldDefaultValue<DGlobalTemplateFieldUpdate>
-            name="defaultValue"
-            control={control}
-         />
-         {OPTION_TYPES.includes(type) && (
-            <TemplateFieldSelectOptions<DGlobalTemplateFieldUpdate>
-               name="options"
-               control={control}
-            />
-         )}
+         {renderDefaultValue()}
+         {renderSelectedOptions()}
          <TemplateFieldDescription<DGlobalTemplateFieldUpdate>
             name="description"
             control={control}
