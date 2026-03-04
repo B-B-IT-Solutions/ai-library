0e62ca45681e3b9d2dff63da4fbb5aff37caca57
Sergej Bobusky <bobuskysergej@gmail.com>
2026-03-04 10:02:35 +0100

    init commit

--- src/components/prompts/dialogs/create-prompt-preview-dialog.tsx
+++ src/components/prompts/dialogs/create-prompt-preview-dialog.tsx
@@ -11,8 +11,7 @@ import {
 } from "@/components/shadcn/dialog";
 import { CallbackFn } from "@/data/types/common";
 import { DPromptUpdate } from "@/data/types/domain/prompt";
-
-import { PromptEdit } from "./prompt-edit";
+import { PromptEdit } from "../prompt/prompt-edit";
 
 type Props = {
    onCancel: CallbackFn;
