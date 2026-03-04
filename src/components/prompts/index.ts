b41d880e2bada8966667ce28c809e15e568ef54a
Sergej Bobusky <bobuskysergej@gmail.com>
2026-03-04 10:47:01 +0100

    init commit

--- src/components/prompts/index.ts
+++ src/components/prompts/index.ts
@@ -3,7 +3,6 @@ export { PromptView as PromptFormView } from "./prompt/prompt-view";
 export { PromptsDashboard } from "./prompts-dashboard";
 export { CreatePromptPreviewDialog } from "./dialogs/create-prompt-preview-dialog";
 export { PromptListItem } from "./lists/items/prompt-list-item";
-export { PromptsEmpty } from "./prompts-empty";
 export { PromptsList } from "./lists/prompts-list";
 export { PromptView } from "./prompt/prompt-view";
 export { PromptCreateEdit } from "./prompt-create-edit";
