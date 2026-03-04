9d62dce2f07f33215bac7057c8d3fb050a168638
Sergej Bobusky <bobuskysergej@gmail.com>
2026-03-04 09:22:19 +0100

    init commit

--- src/components/prompt-templates/index.ts
+++ src/components/prompt-templates/index.ts
@@ -1,6 +1,3 @@
-export { TemplateCard } from "./template-card";
-export { TemplateCards } from "./template-cards";
 export { TemplateFilters } from "./template-filters";
-export { TemplateSelector } from "./template-selector";
 export { TemplateFieldForm } from "./fields/template-fields-form";
 export { CreateTemplateFieldsFormDialog } from "./dialogs/create-template-fields-form-dialog";
--- src/components/prompts/prompt-create-edit.tsx
+++ src/components/prompts/prompt-create-edit.tsx
@@ -1,20 +1,9 @@
-"use client";
-
-import { FC, useState } from "react";
-
-import { TemplateSelector } from "@/components/prompt-templates";
 import { PromptEdit } from "@/components/prompts";
-import { DPromptTemplateDescriptor } from "@/data/types/domain/prompt.template";
 
-export const PromptCreateEdit: FC = () => {
-   const [template, setTemplate] = useState<
-      DPromptTemplateDescriptor | undefined
-   >();
-
+export const PromptCreateEdit = () => {
    const editForm = () => {
       return (
          <div className="space-y-6">
-            <TemplateSelector onSelect={setTemplate} />
             <PromptEdit mode="create" />
          </div>
       );
