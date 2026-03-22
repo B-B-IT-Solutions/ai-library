377ee407ec6f2d86e7f6a98b078298944069d301
Sergej Bobusky <bobuskysergej@gmail.com>
2026-03-22 19:29:23 +0100

    init commit

--- prisma/migrations/20260317000000_add_user_id_to_prompt_descriptor/migration.sql
+++
@@ -1,8 +0,0 @@
--- AlterTable
-ALTER TABLE "prompt_descriptor" ADD COLUMN "user_id" UUID NOT NULL;
-
--- CreateIndex
-CREATE INDEX "prompt_descriptor_user_id_idx" ON "prompt_descriptor"("user_id");
-
--- AddForeignKey
-ALTER TABLE "prompt_descriptor" ADD CONSTRAINT "prompt_descriptor_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "user"("id") ON DELETE CASCADE ON UPDATE CASCADE;
