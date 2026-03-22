906f2c63142bc099d8f96865dbdf866761fe7714
Sergej Bobusky <bobuskysergej@gmail.com>
2026-03-22 09:25:49 +0100

    init commit

--- __mocks__/remark.ts
+++ __mocks__/remark.ts
@@ -1,10 +1,8 @@
 const remarkInstance = {
    use: jest.fn().mockImplementation(() => remarkInstance),
-   processSync: jest
-      .fn()
-      .mockImplementation((text: string) => ({
-         toString: jest.fn().mockImplementation(() => text),
-      })),
+   processSync: jest.fn().mockImplementation((text: string) => ({
+      toString: jest.fn().mockImplementation(() => text),
+   })),
 };
 
 export const remark = jest.fn(() => remarkInstance);
