363bd2527d3d87981206f83b51d62f025d18d8a5
Sergej Bobusky <bobuskysergej@gmail.com>
2026-03-22 13:59:29 +0100

    init commit

--- src/components/library/library-entry/edit/library-entry-edit.tsx
+++ src/components/library/library-entry/edit/library-entry-edit.tsx
@@ -42,7 +42,7 @@ export const LibraryEntryEdit = ({ entry, globalFields }: Props) => {
          className="flex h-screen flex-col bg-slate-50"
          data-testid="library-entry-edit"
       >
-         <div className="border border-slate-200 bg-white px-6 py-4">
+         <div className="border-b border-slate-200 bg-white px-6 py-4">
             {header()}
          </div>
          <div className="flex-1 overflow-y-auto">
