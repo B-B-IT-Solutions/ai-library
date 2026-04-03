0e75a83aa4b216d59ea91907fadbd983a9837611
Sergej Bobusky <bobuskysergej@gmail.com>
2026-04-03 12:56:13 +0200

    init commit

--- src/components/shared/sidebar/sidebar.test.tsx
+++ src/components/shared/sidebar/sidebar.test.tsx
@@ -39,7 +39,7 @@ const assertHeader = () => {
 const assertMenuItems = () => {
    const groupPrompts = screen.getByTestId("group-prompts");
    const prompts = screen.getByTestId("menu-item-prompts");
-   const library = screen.getByTestId("menu-item-library");
+   const templates = screen.getByTestId("menu-item-templates");
 
    const groupLibrary = screen.getByTestId("group-library");
    const marketplace = screen.getByTestId("menu-item-marketplace");
@@ -51,7 +51,7 @@ const assertMenuItems = () => {
 
    assertInDocument(groupPrompts);
    assertInDocument(prompts);
-   assertInDocument(library);
+   assertInDocument(templates);
 
    assertInDocument(groupLibrary);
    assertInDocument(marketplace);
