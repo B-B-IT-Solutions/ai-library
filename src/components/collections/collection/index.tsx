f4ca57ddf45e954f719547a386d05abd8270ae4a
Sergej Bobusky <bobuskysergej@gmail.com>
2026-04-05 10:05:24 +0200

    init commit

--- src/components/collections/collection/index.tsx
+++ src/components/collections/collection/index.tsx
@@ -1,4 +1,2 @@
-export { CreateCollectionForm } from "./edit/create-collection-form";
-export { CollectionEditForm as CollectionEditForm } from "./edit/collection-edit-form";
 export { CollectionEdit } from "./edit/collection-edit";
 export { CollectionView } from "./view/collection-view";
