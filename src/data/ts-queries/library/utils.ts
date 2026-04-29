f33f5c70c99ee1ea4b0e2fac608e06114c05e064
Sergej Bobusky <bobuskysergej@gmail.com>
2026-04-29 19:44:36 +0200

    init commit

--- src/data/ts-queries/library/utils.ts
+++ src/data/ts-queries/library/utils.ts
@@ -8,5 +8,4 @@ export const libraryKeys = {
    entryCollections: (entryId: string) =>
       [...libraryKeys.all, "entry", entryId, "collections"] as const,
    collections: () => [...libraryKeys.all, "collections"] as const,
-   collection: (id: string) => [...libraryKeys.all, "collection", id] as const,
 };
