4f1ab318f4150b705e76ba9567a747d8c9ca6ef3
Sergej Bobusky <bobuskysergej@gmail.com>
2026-05-01 12:22:28 +0200

    init commit

--- src/data/actions/template/template.public.actions.ts
+++ src/data/actions/template/template.public.actions.ts
@@ -1,27 +1,18 @@
 "use server";
 
+import { validate as isValidUuid } from "uuid";
+
 import { EMPTY_PAGE, formatError } from "@/data/actions/utils";
 import prisma from "@/data/repositories/prisma";
 import { ServiceFactory } from "@/data/services";
 import { DbClient } from "@/data/types/db/common";
 import {
+   DPromptTemplate,
    DPromptTemplateDataPromptGeneration,
    DPromptTemplateDescriptor,
    DTemplateDescriptorsPage,
    DTemplateDescriptorsPageQuery,
 } from "@/data/types/domain/prompt.template";
-
-export const getPublicTemplateDescriptor = async (
-   descriptorId: string
-): Promise<DPromptTemplateDescriptor | null> => {
-   try {
-      const service = getService();
-      return await service.getPublicTemplateDescriptor(descriptorId);
-   } catch (error) {
-      console.error(formatError(error));
-      return null;
-   }
-};
 
 export const getPublicTemplateDescriptorsPage = async (
    query: DTemplateDescriptorsPageQuery
@@ -35,10 +26,46 @@ export const getPublicTemplateDescriptorsPage = async (
    }
 };
 
+export const getPublicTemplateDescriptor = async (
+   descriptorId: string
+): Promise<DPromptTemplateDescriptor | null> => {
+   try {
+      if (!isValidUuid(descriptorId)) {
+         throw new Error("Invalid Descriptor ID.");
+      }
+
+      const service = getService();
+      return await service.getPublicTemplateDescriptor(descriptorId);
+   } catch (error) {
+      console.error(formatError(error));
+      return null;
+   }
+};
+
+export const getPublicPromptTemplate = async (
+   templateId: string
+): Promise<DPromptTemplate | null> => {
+   try {
+      if (!isValidUuid(templateId)) {
+         throw new Error("Invalid Template ID.");
+      }
+
+      const service = getService();
+      return await service.getPublicPromptTemplate(templateId);
+   } catch (error) {
+      console.error(formatError(error));
+      return null;
+   }
+};
+
 export const getPublicPromptGenerationTemplateData = async (
    templateId: string
 ): Promise<DPromptTemplateDataPromptGeneration | null> => {
    try {
+      if (!isValidUuid(templateId)) {
+         throw new Error("Invalid Descriptor ID.");
+      }
+
       const service = getService();
       return await service.getPublicTemplateDataForPromptGeneration(templateId);
    } catch (error) {
