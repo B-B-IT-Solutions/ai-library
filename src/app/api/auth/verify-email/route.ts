3e506b47586b18ad9be881b18f703938f0683d3f
Sergej Bobusky <bobuskysergej@gmail.com>
2026-05-04 10:32:55 +0200

    init commit

--- src/app/api/auth/verify-email/route.ts
+++ src/app/api/auth/verify-email/route.ts
@@ -3,6 +3,7 @@ import { type NextRequest } from "next/server";
 
 import prisma from "@/data/repositories/prisma";
 import { ServiceFactory } from "@/data/services";
+import { DbClient } from "@/data/types/db/common";
 
 export async function GET(request: NextRequest) {
    const searchParams = request.nextUrl.searchParams;
@@ -10,19 +11,28 @@ export async function GET(request: NextRequest) {
    const email = searchParams.get("email");
 
    if (!token || !email) {
-      redirect("/auth/sign-in?error=invalid_link");
+      return redirect("/auth/sign-in?error=invalid_link");
    }
 
-   const factory = new ServiceFactory(prisma);
-   const tokenService = factory.getVerificationTokenService();
+   const tokenService = getTokenService();
    const isValid = await tokenService.verifyToken(email, token);
 
    if (!isValid) {
-      redirect("/auth/sign-in?error=expired_link");
+      return redirect("/auth/sign-in?error=expired_link");
    }
 
-   const userService = factory.getUserService();
+   const userService = getUserService();
    await userService.verifyEmail(email);
 
    redirect("/auth/sign-in?verified=true");
 }
+
+const getTokenService = (dbClient: DbClient = prisma) => {
+   const factory = new ServiceFactory(dbClient);
+   return factory.getVerificationTokenService();
+};
+
+const getUserService = (dbClient: DbClient = prisma) => {
+   const factory = new ServiceFactory(dbClient);
+   return factory.getUserService();
+};
