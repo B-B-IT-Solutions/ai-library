ff51fcc685a21462009a0ef475393b3011a1955d
Sergej Bobusky <bobuskysergej@gmail.com>
2026-05-04 14:10:40 +0200

    init commit

--- src/data/repositories/user/user.mapper.test.ts
+++ src/data/repositories/user/user.mapper.test.ts
@@ -13,6 +13,7 @@ export const toDUserInternatTest = (pUser: User): DUserInternal => {
       role: pUser.role,
       password: pUser.password,
       stripeCustomerId: pUser.stripeCustomerId,
+      emailVerified: pUser.emailVerified?.toISOString() ?? null,
       updatedAt: pUser.updatedAt.toISOString(),
       createdAt: pUser.createdAt.toISOString(),
    };
@@ -25,6 +26,15 @@ describe("toDUserInternal tests", () => {
 
    it("toDUserInternal test", async () => {
       const user = ptestData.pUser();
+      const result = toDUserInternal(user);
+      const expectedResult = toDUserInternatTest(user);
+      expect(result).toEqual(expectedResult);
+   });
+
+   it("toDUserInternal - emailVerified null - test", async () => {
+      const user = ptestData.pUser();
+      user.emailVerified = null;
+
       const result = toDUserInternal(user);
       const expectedResult = toDUserInternatTest(user);
       expect(result).toEqual(expectedResult);
