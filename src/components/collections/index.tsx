bef815f3a57c9635ddb75314ed5575118069caa0
Sergej Bobusky <bobuskysergej@gmail.com>
2026-05-11 21:38:14 +0200

    init commit

--- src/components/collections/index.tsx
+++ src/components/collections/index.tsx
@@ -4,8 +4,4 @@ export {
 } from "./dialogs";
 export { CollectionsDashboard } from "./collections-dashboard";
 export { collectionsSearchParamsCache } from "./collections-search-params";
-export {
-   CollectionEdit,
-   CollectionView,
-   CollectionViewPublic,
-} from "./collection";
+export { CollectionEdit, CollectionView, CollectionViewPublic } from "./detail";
