a3b78b840b44b2e0efc96a7c92f3d097e246b0b7
Sergej Bobusky <bobuskysergej@gmail.com>
2026-05-14 16:10:22 +0200

    init commit

--- src/components/shared/widgets/form-select.tsx
+++ src/components/shared/widgets/form-select.tsx
@@ -14,6 +14,7 @@ import {
 import {
    Select,
    SelectContent,
+   SelectGroup,
    SelectItem,
    SelectTrigger,
    SelectValue,
@@ -79,20 +80,28 @@ export const FormSelect = <T extends FieldValues>({
             <FormItem data-testid={name}>
                {renderlabel()}
                {renderDescription()}
-               <Select value={field.value} onValueChange={field.onChange}>
+               <div className="relative">
                   <FormControl>
-                     <SelectTrigger className="w-full">
-                        <SelectValue placeholder={placeholder} />
-                     </SelectTrigger>
+                     <Select
+                        name={field.name}
+                        value={field.value}
+                        onValueChange={field.onChange}
+                     >
+                        <SelectTrigger className="w-full">
+                           <SelectValue placeholder={placeholder} />
+                        </SelectTrigger>
+                        <SelectContent>
+                           <SelectGroup>
+                              {map(options, (o, idx) => (
+                                 <SelectItem key={idx} value={getValue(o)}>
+                                    {getLabel(o)}
+                                 </SelectItem>
+                              ))}
+                           </SelectGroup>
+                        </SelectContent>
+                     </Select>
                   </FormControl>
-                  <SelectContent>
-                     {map(options, (o, idx) => (
-                        <SelectItem key={idx} value={getValue(o)}>
-                           {getLabel(o)}
-                        </SelectItem>
-                     ))}
-                  </SelectContent>
-               </Select>
+               </div>
                <FormMessage />
             </FormItem>
          )}
