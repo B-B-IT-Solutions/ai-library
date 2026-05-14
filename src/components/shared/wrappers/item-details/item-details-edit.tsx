bc310cce28c0bc2806829a3a3f775c92ab9288ca
Sergej Bobusky <bobuskysergej@gmail.com>
2026-05-14 14:02:24 +0200

    init commit

--- src/components/shared/wrappers/item-details/item-details-edit.tsx
+++ src/components/shared/wrappers/item-details/item-details-edit.tsx
@@ -4,7 +4,7 @@ type Props = PropsWithChildren<{ "data-testid"?: string }>;
 
 const ItemDetailsEdit = ({ children, "data-testid": testId }: Props) => {
    return (
-      <div className="flex h-screen flex-col bg-slate-50" data-testid={testId}>
+      <div className="flex flex-col bg-slate-50" data-testid={testId}>
          {children}
       </div>
    );
