1bc06abd0bdda9617569056cda4082588d451556
Sergej Bobusky <bobuskysergej@gmail.com>
2026-05-19 02:23:08 +0200

    init commit

--- src/data/actions/subscription/subscription.actions.test.ts
+++ src/data/actions/subscription/subscription.actions.test.ts
@@ -1,18 +1,12 @@
 jest.mock("@/data/services/subscription");
 jest.mock("@/data/actions/auth-utils");
-jest.mock("next/cache");
 
 import { dtestData } from "@tests";
-import { revalidatePath } from "next/cache";
 
 import { requireUser } from "@/data/actions/auth-utils";
 import { SubscriptionService } from "@/data/services/subscription";
-import { DTrialStatus } from "@/data/types/domain/subscription";
-import { ActionResult } from "@/data/types/utils";
 
 import {
-   chooseFreeplan,
-   getHasActiveAccess,
    getSubscription,
    getSubscriptionPlans,
    getTrialStatus,
@@ -20,9 +14,7 @@ import {
 
 const sGetAvailablePlans = SubscriptionService.prototype.getAvailablePlans;
 const sGetSubscription = SubscriptionService.prototype.getSubscription;
-const sHasActiveAccess = SubscriptionService.prototype.hasActiveAccess;
 const sGetTrialStatus = SubscriptionService.prototype.getTrialStatus;
-const sSetPlanChosen = SubscriptionService.prototype.setPlanChosen;
 
 const sGetAvailablePlansMock = sGetAvailablePlans as jest.MockedFunction<
    typeof sGetAvailablePlans
@@ -30,18 +22,8 @@ const sGetAvailablePlansMock = sGetAvailablePlans as jest.MockedFunction<
 const sGetSubscriptionMock = sGetSubscription as jest.MockedFunction<
    typeof sGetSubscription
 >;
-const sHasActiveAccessMock = sHasActiveAccess as jest.MockedFunction<
-   typeof sHasActiveAccess
->;
 const sGetTrialStatusMock = sGetTrialStatus as jest.MockedFunction<
    typeof sGetTrialStatus
->;
-const sSetPlanChosenMock = sSetPlanChosen as jest.MockedFunction<
-   typeof sSetPlanChosen
->;
-
-const revalidatePathMock = revalidatePath as jest.MockedFunction<
-   typeof revalidatePath
 >;
 
 const requireUserMock = requireUser as jest.MockedFunction<typeof requireUser>;
@@ -109,55 +91,6 @@ describe("getSubscription tests", () => {
    });
 });
 
-describe("getHasActiveAccess tests", () => {
-   beforeEach(() => {
-      jest.clearAllMocks();
-      jest.spyOn(console, "error").mockImplementation(() => {});
-   });
-
-   afterEach(() => {
-      jest.restoreAllMocks();
-   });
-
-   it("user undefined - test", async () => {
-      const error = new Error("Unknown user");
-      requireUserMock.mockRejectedValue(error);
-
-      const result = await getHasActiveAccess();
-
-      expect(result).toBe(false);
-      expect(requireUserMock).toHaveBeenCalledTimes(1);
-      expect(sHasActiveAccessMock).not.toHaveBeenCalled();
-      expect(console.error).toHaveBeenCalledTimes(1);
-   });
-
-   it("active access - true - test", async () => {
-      const user = dtestData.dLoginUser();
-      requireUserMock.mockResolvedValue(user);
-      sHasActiveAccessMock.mockResolvedValue(true);
-
-      const result = await getHasActiveAccess();
-
-      expect(result).toBe(true);
-      expect(requireUserMock).toHaveBeenCalledTimes(1);
-      expect(sHasActiveAccessMock).toHaveBeenCalledTimes(1);
-      expect(sHasActiveAccessMock).toHaveBeenCalledWith(user.id);
-   });
-
-   it("active access - false - test", async () => {
-      const user = dtestData.dLoginUser();
-      requireUserMock.mockResolvedValue(user);
-      sHasActiveAccessMock.mockResolvedValue(false);
-
-      const result = await getHasActiveAccess();
-
-      expect(result).toBe(false);
-      expect(requireUserMock).toHaveBeenCalledTimes(1);
-      expect(sHasActiveAccessMock).toHaveBeenCalledTimes(1);
-      expect(sHasActiveAccessMock).toHaveBeenCalledWith(user.id);
-   });
-});
-
 describe("getTrialStatus tests", () => {
    beforeEach(() => {
       jest.clearAllMocks();
@@ -193,70 +126,5 @@ describe("getTrialStatus tests", () => {
       expect(requireUserMock).toHaveBeenCalledTimes(1);
       expect(sGetTrialStatusMock).toHaveBeenCalledTimes(1);
       expect(sGetTrialStatusMock).toHaveBeenCalledWith(user.id);
-   });
-});
-
-describe("chooseFreeplan tests", () => {
-   beforeEach(() => {
-      jest.clearAllMocks();
-      jest.spyOn(console, "error").mockImplementation(() => {});
-   });
-
-   afterEach(() => {
-      jest.restoreAllMocks();
-   });
-
-   it("user undefined - test", async () => {
-      const error = new Error("Unknown user");
-      requireUserMock.mockRejectedValue(error);
-
-      const result = await chooseFreeplan();
-      const expectedResult: ActionResult = {
-         success: false,
-         message: "Fehler beim Wählen des Plans",
-      };
-
-      expect(result).toEqual(expectedResult);
-      expect(requireUserMock).toHaveBeenCalledTimes(1);
-      expect(sSetPlanChosenMock).not.toHaveBeenCalled();
-      expect(console.error).toHaveBeenCalledTimes(1);
-   });
-
-   it("error - test", async () => {
-      const user = dtestData.dLoginUser();
-      requireUserMock.mockResolvedValue(user);
-
-      const dbError = new Error("db error");
-      sSetPlanChosenMock.mockRejectedValue(dbError);
-
-      const result = await chooseFreeplan();
-
-      const expectedResult: ActionResult = {
-         success: false,
-         message: "Fehler beim Wählen des Plans",
-      };
-
-      expect(result).toEqual(expectedResult);
-      expect(requireUserMock).toHaveBeenCalledTimes(1);
-      expect(sSetPlanChosenMock).toHaveBeenCalledTimes(1);
-      expect(sSetPlanChosenMock).toHaveBeenCalledWith(user.id);
-   });
-
-   it("success - test", async () => {
-      const user = dtestData.dLoginUser();
-      requireUserMock.mockResolvedValue(user);
-      sSetPlanChosenMock.mockResolvedValue(undefined);
-
-      const result = await chooseFreeplan();
-      const expectedResult: ActionResult = {
-         success: true,
-         message: "Plan erfolgreich gewählt",
-      };
-
-      expect(result).toEqual(expectedResult);
-      expect(requireUserMock).toHaveBeenCalledTimes(1);
-      expect(sSetPlanChosenMock).toHaveBeenCalledTimes(1);
-      expect(sSetPlanChosenMock).toHaveBeenCalledWith(user.id);
-      expect(revalidatePathMock).toHaveBeenCalledWith("/", "layout");
    });
 });
