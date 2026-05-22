595c9d3f6297575f687e6ec9b2ad78bc1ece1a04
Sergej Bobusky <bobuskysergej@gmail.com>
2026-05-22 10:34:59 +0200

    init commit

--- prisma/migrations/20260519001543_version_0_0_6/migration.sql
+++ prisma/migrations/20260519001543_version_0_0_6/migration.sql
@@ -2,5 +2,5 @@
 ALTER TABLE "user" ADD COLUMN     "trial_ends_at" TIMESTAMP(6);
 
 UPDATE "user"
-SET "trial_ends_at" = "2026-06-30T00:00:00.000Z"
+SET "trial_ends_at" = '2026-06-30T00:00:00.000Z'
 WHERE "trial_ends_at" IS NULL;
