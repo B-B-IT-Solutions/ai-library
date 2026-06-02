d4f86c5aeb7b7cd9b1b83b965ff8268087111d10
Sergej Bobusky <bobuskysergej@gmail.com>
2026-06-02 15:22:39 +0200

    init commit

--- __mocks__/@dnd-kit/core.d.ts
+++
@@ -1,8 +0,0 @@
-type MockDragEndEvent = {
-   active: { id: string | number };
-   over: { id: string | number } | null;
-};
-
-declare module "@dnd-kit/core" {
-   export function triggerDragEnd(event: MockDragEndEvent): void;
-}
