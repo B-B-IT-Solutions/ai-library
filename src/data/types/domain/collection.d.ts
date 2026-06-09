837600b0ea16297323460e0c40ac3df8d207d4a2
Sergej Bobusky <bobuskysergej@gmail.com>
2026-06-09 13:07:32 +0200

    init commit

--- src/data/types/domain/collection.d.ts
+++ src/data/types/domain/collection.d.ts
@@ -1,8 +1,14 @@
 import z from "zod";
 
 import { updateCollectionSchema } from "@/data/types/validators/collection";
+import { Page, PageQuery } from "../common";
 
-export type DCollectionUpdate = z.infer<typeof updateCollectionSchema>;
+export type DCollectionsPageQuery = PageQuery<DCollectionsFilter>;
+export type DCollectionsPage = Page<DCollection>;
+
+export type DCollectionsFilter = {
+   search?: string;
+};
 
 export type DCollection = {
    id: string;
@@ -29,3 +35,5 @@ export type DCollectionEntry = {
    promptId: string;
    addedAt: string;
 };
+
+export type DCollectionUpdate = z.infer<typeof updateCollectionSchema>;
