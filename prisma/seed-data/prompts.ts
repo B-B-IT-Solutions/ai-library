a6407f9894f1f974b3c962fdf7bcef01bd345cdc
Sergej Bobusky <bobuskysergej@gmail.com>
2026-06-30 11:47:32 +0200

    init commit

--- prisma/seed-data/prompts.ts
+++ prisma/seed-data/prompts.ts
@@ -3,6 +3,7 @@ import { map } from "es-toolkit/compat";
 import { PromptCreateInput } from "@/generated/prisma/models";
 
 export const SEED_USER_EMAIL = "test123@gmail.com";
+export const SEED_ADMIN_EMAIL = "admin123@gmail.com";
 
 const promptCategories = (userId: string, categories: string[]) => {
    return map(categories, (cat: string) => {
