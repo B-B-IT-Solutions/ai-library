dac9876564b8eb1b5c2b38c3b81a244d3a4c0bdd
Sergej Bobusky <bobuskysergej@gmail.com>
2026-07-01 11:43:17 +0200

    init commit

--- prisma/migrations/20260701082212_version_0_0_9/migration.sql
+++ prisma/migrations/20260701082212_version_0_0_9/migration.sql
@@ -4,3 +4,7 @@ CREATE TYPE "UserRole" AS ENUM ('USER', 'PROMO_USER', 'ADMIN');
 -- AlterTable
 ALTER TABLE "user" DROP COLUMN "role",
 ADD COLUMN     "role" "UserRole" NOT NULL DEFAULT 'USER';
+
+UPDATE "user"
+SET "role" = "ADMIN"
+WHERE "email" = "bobuskysergej@gmail.com";
