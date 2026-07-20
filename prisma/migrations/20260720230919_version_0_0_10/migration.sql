2b120763984dfe4649284b271ac3703a7dfe5951
Sergej Bobusky <bobuskysergej@gmail.com>
2026-07-21 01:10:24 +0200

    init commit

---
+++ prisma/migrations/20260720230919_version_0_0_10/migration.sql
@@ -0,0 +1,30 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `recommended_model` on the `prompt` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "prompt" DROP COLUMN "recommended_model",
+ADD COLUMN     "model_id" INTEGER;
+
+-- CreateTable
+CREATE TABLE "prompt_model" (
+    "id" SERIAL NOT NULL,
+    "user_id" UUID NOT NULL,
+    "name" VARCHAR(250) NOT NULL,
+
+    CONSTRAINT "prompt_model_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE INDEX "prompt_model_user_id_idx" ON "prompt_model"("user_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "prompt_model_user_id_name_key" ON "prompt_model"("user_id", "name");
+
+-- AddForeignKey
+ALTER TABLE "prompt" ADD CONSTRAINT "prompt_model_id_fkey" FOREIGN KEY ("model_id") REFERENCES "prompt_model"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "prompt_model" ADD CONSTRAINT "prompt_model_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "user"("id") ON DELETE CASCADE ON UPDATE CASCADE;
